30e838325b86db20c59cf7b56e4af8bd72d075be
lvdat <lvdat13@gmail.com>
2024-11-03 18:44:42 +0700

    init vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Navbar from './components/Navbar.vue'
+</script>
+<template>
+    <div class="main">
+        <Navbar />
+        <router-view />
+    </div>
+</template>
+<script>
+export default {
+    name: 'kMovie',
+    watch: {
+        $route: {
+            immediate: true,
+            handler(to, from) {
+                document.title = to.meta.title
+                    ? to.meta.title + ' | kMovie'
+                    : 'kMovie'
+                this.viewChapMode = to.name === 'comicview'
+            },
+        },
+    },
+}
+</script>
